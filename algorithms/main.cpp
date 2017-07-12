348ef112dc58deb1f91b8005fbffb75577cbc038
NAMYUNWOO <nyw8902@gmail.com>
2017-07-12 22:38:17 +0900

    Initial Commit

---
+++ algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithms
+//
+//  Created by yunwoo on 2017. 7. 12..
+//  Copyright © 2017년 yunwoo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
